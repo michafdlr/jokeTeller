4fafeedda2a3f31f45ee70455a23885a5740abc7
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-06-14 19:19:54 +0200

    added language options

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 const audioElement = document.getElementById('audio')
 const btnJoke = document.getElementById('button')
+const langSelector = document.getElementById('lang-selector')
 
 const apiKey = getTtsKey();
-let hl = 'de-de'
+let hl = ''
 let codec = 'MP3'
-let src = 'Hallo Welt!'
+let src = ''
+let language = ''
+let jokeLanguage = ''
 
-const jokeUrl = 'https://v2.jokeapi.dev/joke/Any?lang=de&blacklistFlags=racist'
+const setLanguage = () => {
+  language = langSelector.value
+}
 
 const getJoke = async () => {
+  if (language === 'german') {
+    jokeLanguage = 'de'
+  } else {
+    jokeLanguage = 'en'
+  }
+  const jokeUrl = `https://v2.jokeapi.dev/joke/Any?lang=${jokeLanguage}&blacklistFlags=racist`
   const response = await fetch(jokeUrl);
   const data  = await response.json();
   console.log(data.delivery)
   if (!data.error && typeof data.delivery != 'undefined') {
     src = data.delivery;
   } else {
-    src = "Ich fand leider keinen Joke.";
+    if (language === 'german') {
+      src = "Ich fand leider keinen Joke.";
+    } else {
+      src = 'No joke found.'
+    }
   }
 }
 
 const transformToSpeech = async () => {
+  if (language === 'german') {
+    hl = 'de-de'
+  } else {
+    hl = 'en-us'
+  }
   const voiceUrl = `http://api.voicerss.org/?key=${apiKey}&hl=${hl}&c=${codec}&src=${src}`
   const response = await fetch(voiceUrl);
   audioElement.src = response.url;
@@ -30,3 +50,5 @@ btnJoke.addEventListener('click', async () => {
   await getJoke();
   await transformToSpeech();
 })
+
+langSelector.addEventListener('change', setLanguage)
